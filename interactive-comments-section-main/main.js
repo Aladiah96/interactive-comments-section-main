4e656a7e90d031b46d4a6c4f1735831d65242bdb
Wagner Almeida <63356947+Aladiah96@users.noreply.github.com>
2022-03-13 20:10:37 -0300

    Added comments

--- interactive-comments-section-main/main.js
+++ interactive-comments-section-main/main.js
@@ -15,6 +15,18 @@ $( document ).ready(function() {
         '<p class="comment__score">' + comment.score + '</p><img src="./images/icon-minus.svg" alt=""></div><div class="comment__container"><div class="comment__container__header">' +
         '<h2 class="comment__profile"><img src = ' + comment.user.image.png + '>' + comment.user.username + '</h2><p class="comment__creation">' + comment.createdAt + '</p><img src="./images/icon-reply.svg" alt="" class="reply">' +
         '</div><p class="comment__content">' + comment.content + '</p></div></div>')
+
+      if(comment.replies.length){
+        for (let i = 0; i < comment.replies.length; i++){
+
+          const reply = comment.replies[i];
+
+          $('.row').append('<div class="col-lg-12 comment"><div class="comment__votes"><img src="./images/icon-plus.svg" alt="">' +
+            '<p class="comment__score">' + reply.score + '</p><img src="./images/icon-minus.svg" alt=""></div><div class="comment__container"><div class="comment__container__header">' +
+            '<h2 class="comment__profile"><img src = ' + reply.user.image.png + '>' + reply.user.username + '</h2><p class="comment__creation">' + reply.createdAt + '</p><img src="./images/icon-reply.svg" alt="" class="reply">' +
+            '</div><p class="comment__content">' + reply.content + '</p></div></div>')
+        }
+      }
     }
   });
 });

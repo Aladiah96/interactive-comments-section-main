2ca6afa5f110ebaf0c3733ae28de65a150179375
Wagner Almeida <63356947+Aladiah96@users.noreply.github.com>
2022-03-13 11:30:11 -0300

    Managed to create 2 containers
    The next challenge is to assign each object to it's own place.

--- interactive-comments-section-main/main.js
+++ interactive-comments-section-main/main.js
@@ -7,15 +7,19 @@ $( document ).ready(function() {
   const comments = data.comments;
   const commentQty = Object.entries(comments).length;
   
-for (let i = 0; i < commentQty; i++) {
-  console.log(i);
-  $('.row').html('<div class="col-lg-6 comment"><div class="comment__votes"><img src="./images/icon-plus.svg" alt=""><p class="comment__score"></p><img src="./images/icon-minus.svg" alt=""></div><div class="comment__container"><div class="comment__container__header"><h2 class="comment__profile"></h2><p class="comment__creation"></p><img src="./images/icon-reply.svg" alt="" class="reply"></div><p class="comment__content"></p></div></div>')
-  $(".comment__profile").text(data.comments[i].user.username);
-  $(".comment__creation").text(data.comments[i].createdAt);
-  $(".comment__score").text(data.comments[i].score);
-  $(".comment__content").text(data.comments[i].content);
-  $('.comment__profile').prepend($('<img>',{class:'comment_profilepic',src:data.comments[i].user.image.png}))
+    for (let i = 0; i < commentQty; i++) {
+      
+      const comment = data.comments[i];
+
+      $('.row').append('<div class="col-lg-12 comment"><div class="comment__votes"><img src="./images/icon-plus.svg" alt="">' +
+        '<p class="comment__score"></p><img src="./images/icon-minus.svg" alt=""></div><div class="comment__container"><div class="comment__container__header">' +
+        '<h2 class="comment__profile cp"></h2><p class="comment__creation"></p><img src="./images/icon-reply.svg" alt="" class="reply"></div><p class="comment__content"></p></div></div>')
+      $(".comment__profile").text(comment.user.username);
+      $(".comment__creation").text(comment.createdAt);
+      $(".comment__score").text(comment.score);
+      $(".comment__content").text(comment.content);
+      $('.comment__profile').prepend($('<img>',{class:'comment_profilepic', src:comment.user.image.png}))  
   
-}
+    }
   });
 });

5c08c4d525cc9acdd1d8ab0b7adacbd086a2085e
Wagner Almeida <63356947+Aladiah96@users.noreply.github.com>
2022-03-26 17:48:49 -0300

    Added the new comment area and functionality to the reply button

--- interactive-comments-section-main/main.js
+++ interactive-comments-section-main/main.js
@@ -16,11 +16,9 @@ $( document ).ready(function() {
         '<h2 class="comment__profile"><img src = ' + comment.user.image.png + '>' + comment.user.username + '</h2><p class="comment__creation">' + comment.createdAt + '</p><img src="./images/icon-reply.svg" alt="" class="reply">' +
         '</div><p class="comment__content">' + comment.content + '</p></div></div>')
 
-
-        //preciso adicionar a reply ao comentário correspondente.
       if(comment.replies.length){
 
-        $('.id'+ comment.id).after('<div class="replyContainer"></div>')
+        $('.id'+ comment.id).after('<div class="align-items-end flex-column replyContainer"></div>')
         
         for (let i = 0; i < comment.replies.length; i++){
 
@@ -28,10 +26,19 @@ $( document ).ready(function() {
 
           $('.replyContainer').append('<div class="col-lg-11 reply"><div class="reply__votes"><img src="./images/icon-plus.svg" alt="">' +
           '<p class="comment__score">' + reply.score + '</p><img src="./images/icon-minus.svg" alt=""></div><div class="reply__container"><div class="reply__container__header">' +
-          '<h2 class="comment__profile"><img src = ' + reply.user.image.png + '>' + reply.user.username + '</h2><p class="reply__creation">' + reply.createdAt + '</p><img src="./images/icon-reply.svg" alt="" class="reply">' +
+          '<h2 class="comment__profile"><img src = ' + reply.user.image.png + '>' + reply.user.username + '</h2><p class="reply__creation">' + reply.createdAt + '</p><button class="replyBtn"><img src="./images/icon-reply.svg" alt="" class="reply"><p>Reply</p></button> ' +
           '</div><p class="reply__content">' + reply.content + '</p></div></div>')
         }
       }
     }
+    //adicionar comentário
+
+    $('.row').append('<div class="col-12 makeAComment"><img src="'+data.currentUser.image.png+'" alt=""><input type="text" name="comment" id="comment"><input type="button" value="send"></div>')
+
+    //adicionar div de reply
+    $('.replyBtn').on('click', () =>{
+      console.log('passou');
+    })
+    //adicionar modal do delete
   });
 });
